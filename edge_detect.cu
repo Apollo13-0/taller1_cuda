7ed668b32323b46f8a38fb4d62c8851ea16d088c
Erick Barrantes <erick.cr12@outlook.com>
2024-09-12 17:36:16 -0600

    Se elimina codigo que no se utilizaba

--- edge_detect.cu
+++ edge_detect.cu
@@ -5,11 +5,9 @@
 #include "stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-//#include <opencv2/opencv.hpp> // For displaying images
 
 #define SOBEL_FILTER_WIDTH 3
 
-//using namespace cv;
 
 // Define the Sobel kernel for x and y directions
 __constant__ int SobelX[SOBEL_FILTER_WIDTH][SOBEL_FILTER_WIDTH] = {
@@ -24,7 +22,7 @@ __constant__ int SobelY[SOBEL_FILTER_WIDTH][SOBEL_FILTER_WIDTH] = {
     { 1, 2, 1}
 };
 
-// CUDA kernel to apply the Sobel operator
+// This kernel computes the Sobel filter on the input image
 __global__ void sobelFilterKernel(unsigned char *inputImage, unsigned char *outputImage, int width, int height) {
     int x = blockIdx.x * blockDim.x + threadIdx.x; // column
     int y = blockIdx.y * blockDim.y + threadIdx.y; // row
@@ -68,12 +66,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    printf("Image loaded: %ix%i, channels: %x", width, height, channels);
-
-    // Display the image using OpenCV
-    //Mat inputImage(height, width, CV_8UC1, h_inputImage); // Create an OpenCV Mat from the image data
-    //imshow("Original Image", inputImage);  // Show the original image
-    //waitKey(0); // Wait for a key press
+    printf("Image loaded: %ix%i, channels: %x\n", width, height, channels);
 
     clock_t start = clock();
 
@@ -93,22 +86,17 @@ int main(int argc, char* argv[]) {
     dim3 blockDim(16, 16);
     dim3 gridDim((width + blockDim.x - 1) / blockDim.x, (height + blockDim.y - 1) / blockDim.y);
 
-    // Launch the Sobel kernel (make sure the Sobel kernel is defined earlier)
+    // Launch the Sobel kernel
     sobelFilterKernel<<<gridDim, blockDim>>>(d_inputImage, d_outputImage, width, height);
 
     clock_t end = clock();
 
     double time = (double)(end-start)/CLOCKS_PER_SEC;
 
-    printf("GPU time = %fs", time);
+    printf("GPU time = %fs\n", time);
 
     // Copy the result back to host
     cudaMemcpy(h_outputImage, d_outputImage, width * height * sizeof(unsigned char), cudaMemcpyDeviceToHost);
-
-    // Create an OpenCV Mat for the output and display it
-    //Mat outputImage(height, width, CV_8UC1, h_outputImage);
-    //imshow("Sobel Edge Detection", outputImage); // Show the Sobel edge-detected image
-    //waitKey(0); // Wait for a key press
 
     // Save the Sobel result to a file (optional)
     stbi_write_png("sobel_output.png", width, height, 1, h_outputImage, width);
